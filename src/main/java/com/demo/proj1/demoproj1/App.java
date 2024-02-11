e9eddb758e1e51753a5824f6409e33543f8222cb
Akshatha SG <Akshatha21sg@gmail.com>
2024-02-10 20:38:53 -0600

    adding full maven proj to git

---
+++ src/main/java/com/demo/proj1/demoproj1/App.java
@@ -0,0 +1,13 @@
+package com.demo.proj1.demoproj1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
